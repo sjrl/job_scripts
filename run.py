556b5aaf319352262a9c2f59ff6786bf32591de8
Sebastian Lee <sjrl423@gmail.com>
2019-02-28 14:15:07 -0800

    Some more initialization stuff

--- run.py
+++ run.py
@@ -3,11 +3,11 @@
 import os
 import subprocess
 import sys
-sys.path.insert(0,'/home/sjlee/scripts/executableScripts')
+#sys.path.insert(0,'/home/sjlee/scripts/executableScripts')
 from fchk import fileCheck
 
 
-def runInput(run_dict):
+def run(run_dict):
     
     cwd = os.getcwd()
     for key in run_dict:
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     dir_name = sys.argv[1]
     print_info = False
     list_of_sucess, list_of_failure, run_dict = fileCheck(dir_name,print_info)
-    runInput(run_dict)
+    run(run_dict)
     print( str(len(run_dict)) + ' jobs submitted.')
